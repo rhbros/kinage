1515e4cdc1b9481d7712b929e2f4ca625c4f5fbe
nanoemu <fmeyer1196@yahoo.de>
2015-01-20 01:34:30 +0100

    Improved NOTICE / WARNING / ERROR macros

--- core/global.h
+++ core/global.h
@@ -3,8 +3,25 @@
 
 #include <stdio.h>
 
-#define NOTICE(str) printf("\033[0;32m%s:%d: %s (NOTICE)\n\033[0m", __FILE__, __LINE__, str);
-#define WARNING(str) printf("\033[0;35m%s:%d: %s (WARNING)\n\033[0m", __FILE__, __LINE__, str);
-#define ERROR(str) printf("\033[0;31m%s:%d: %s (ERROR)\n\033[0m", __FILE__, __LINE__, str);
+// These macros are quite unsecure and allow for buffer overflows (sprintf)
+// but we assume this won't happen for now...
+
+#define NOTICE(...) { int _line = __LINE__;\
+	char _not[512];\
+	sprintf(_not, __VA_ARGS__);\
+	printf("\033[0;32m%s:%d: %s (NOTICE)\n\033[0m", __FILE__, _line, _not);\
+}
+
+#define WARNING(...) { int _line = __LINE__;\
+	char _not[512];\
+	sprintf(_not, __VA_ARGS__);\
+	printf("\033[0;35m%s:%d: %s (WARNING)\n\033[0m", __FILE__, _line, _not);\
+}
+
+#define ERROR(...) { int _line = __LINE__;\
+	char _not[512];\
+	sprintf(_not, __VA_ARGS__);\
+	printf("\033[0;31m%s:%d: %s (ERROR)\n\033[0m", __FILE__, _line, _not);\
+}
 
 #endif
